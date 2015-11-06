c6c4dcdfff0b398a6f0f387935f07ddff3bb4bc9
Fred Cons <cons@fullsix.com>
2015-11-06 16:42:05 +0100

    Renaming crunchbase index

--- provisioning/files/scripts/load_crunchbase.sh
+++ provisioning/files/scripts/load_crunchbase.sh
@@ -2,16 +2,16 @@
 
 # create schema
 echo "Creating index"
-curl -XPUT http://localhost:9200/companies_db  --data-binary @/etc/crunchbase/mappings.json
+curl -XPUT http://localhost:9200/crunchbase  --data-binary @/etc/crunchbase/mappings.json
 echo " "
 
 # Import data
 echo "Importing data"
-curl -XPUT http://localhost:9200/companies_db/companies/_bulk  --data-binary @/etc/crunchbase/companies.bulk.json
+curl -XPUT http://localhost:9200/crunchbase/companies/_bulk  --data-binary @/etc/crunchbase/companies.bulk.json
 echo " "
 
 #Count docs
 echo "Counting imported docs"
-curl -XGET http://localhost:9200/companies_db/companies/_count
+curl -XGET http://localhost:9200/crunchbase/companies/_count
 echo " "
 
--- reset.sh
+++ reset.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Deleting index companies_db" 
-curl -XDELETE http://localhost:9200/companies_db
+echo "Deleting index crunchbase" 
+curl -XDELETE http://localhost:9200/crunchbase
 echo ""
 
-echo "Creating mapping companies_db" 
-curl -XPUT http://localhost:9200/companies_db --data-binary @provisioning/files/mappings.json
+echo "Creating mapping crunchbase" 
+curl -XPUT http://localhost:9200/crunchbase --data-binary @provisioning/files/mappings.json
 echo ""
 
 echo "Importing data"
-curl -s -XPUT http://localhost:9200/companies_db/companies/_bulk --data-binary @provisioning/files/companies.bulk.json > /dev/null
+curl -s -XPUT http://localhost:9200/crunchbase/companies/_bulk --data-binary @provisioning/files/companies.bulk.json > /dev/null
 
 sleep 2
 echo "Counting companies"
-curl -XGET http://localhost:9200/companies_db/companies/_count
+curl -XGET http://localhost:9200/crunchbase/companies/_count
 echo ""
